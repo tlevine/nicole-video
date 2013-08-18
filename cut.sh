059f4addf7bf5b8d24668dcf100afefe9765b6b8
Thomas Levine <occurrence@thomaslevine.com>
2013-08-18 16:24:34 -0700

    next stuff

--- cut.sh
+++ cut.sh
@@ -10,3 +10,9 @@ ffmpeg -loop 1 -i spreadsheet.png -t 00:00:01.0 output/10.mpg
 
 # "Here's one of our music videos"
 ffmpeg -i beginning.mp4 -ss 00:00:13.0 -t 00:00:02.0 output/20.mpg
+
+# FMS Symphony
+ffmpeg -i fms-symphony-withsound.avi -ss 00:01:00.0 -t 00:00:06.0 output/30.mpg
+
+# "..."
+ffmpeg -i beginning.mp4 -ss 00:00:15.0 -t 00:00:02.0 output/40.mpg

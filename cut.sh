3616463c3aa64fe03c7e11f6319f15aa6c4598c3
Thomas Levine <occurrence@thomaslevine.com>
2013-08-20 21:45:43 -0700

    another cut

--- cut.sh
+++ cut.sh
@@ -15,4 +15,4 @@ ffmpeg -i beginning.mp4 -ss 00:00:13.0 -t 00:00:02.0 output/20.mpg
 ffmpeg -i fms-symphony-withsound.avi -ss 00:01:00.0 -t 00:00:06.0 output/30.mpg
 
 # "..."
-ffmpeg -i beginning.mp4 -ss 00:00:15.0 -t 00:00:02.0 output/40.mpg
+ffmpeg -i beginning.mp4 -ss 00:00:15.0 -t 00:02:15.0 output/40.mpg
